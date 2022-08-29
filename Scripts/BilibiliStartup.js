503a22ef6a567044ebfd1a0f83967b6da37602cb
onnull <1120630538@qq.com>
2022-08-29 13:38:05 +0800

    Update BilibiliStartup.js

--- Scripts/BilibiliStartup.js
+++ Scripts/BilibiliStartup.js
@@ -28,7 +28,7 @@ hostname=api.bilibili.com
 
 
 
-const Group = $prefs.valueForKey('BiliArea_Policy') || 'ʙɪʟɪʙɪʟɪ';
+const Group = $prefs.valueForKey('BiliArea_Policy') || 'Bilibili';
 
 const message = {
     action: "set_policy_state",
